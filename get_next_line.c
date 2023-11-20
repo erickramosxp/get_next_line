35a8701e49136685dbcabb2011dea3c206630262
erickramosxp <erickramossilva@gmail.com>
2023-11-20 20:42:29 -0300

    N

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: erramos <erramos@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 16:32:11 by erramos           #+#    #+#             */
-/*   Updated: 2023/11/17 19:55:26 by erramos          ###   ########.fr       */
+/*   Updated: 2023/11/20 20:42:06 by erramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,38 @@
 #include <fcntl.h>
 #include <stdio.h>
 
-char	*transfer(char *line, char *buff, int size)
+int     check_backslash(char *buff)
 {
-	char	*temp;
+	int	i;
 
-	if (line == 0)
+	i = 0;
+        while (buff[i] != '\0')
+        {
+                if (buff[i] == '\n')
+                        return (0);
+                i++;
+        }
+        return (1);
+}
+
+char	*transfer(char *line, char *buff, int size, int *check)
+{
+	char	*fund_back;
+	char	*temp;
+	int	i;
+	int	len;
+
+	if (!line)
 	{
 		line = (char *)malloc(size * sizeof(char));
-		if (!line)
-			return (NULL);
 		ft_memmove(line, buff, size);
 	}
-	else 
+	else
 	{
 		temp = ft_strdup(line);
 		free(line);
-		line = ft_strjoin(temp, buff);
-		free(temp);
+		line = (char *)malloc(size * sizeof(char));
+
 	}
 	return (line);
 }
@@ -38,30 +53,34 @@ char	*transfer(char *line, char *buff, int size)
 char	*get_next_line(int fd)
 {
 	char	*buff;
-	static	char	*line;
-	static	int	size;
+	char	*line;
+	static char	*buffer;
+	int	rd;
+	int	check;
 
-	size += BUFFER_SIZE;
-	buff = (char *)malloc(BUFFER_SIZE * sizeof(char));
+	rd = 1;
+	check = 1;
+	buff = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buff)
 		return (NULL);
-	read(fd, buff, BUFFER_SIZE);
-	line = transfer(line, buff, size);
-	free(buff);
+	buff[BUFFER_SIZE + 1] = '\0';
+	while (rd > 0 && check)
+	{
+		rd += read(fd, buff, BUFFER_SIZE);
+		line = transfer(line, buff, rd, &check);
+	}
 	return (line);
 }
 
 int	main(void)
 {
 	int	fd;
-	char	*new;
 
 	fd = open("text.txt", O_RDONLY);
-	new = get_next_line(fd);
-	printf("Primeira parte:\n");
-	printf("%s", new);
+	printf("Primeira parte:\n%s",  get_next_line(fd));
 	printf("\nsegunda parte:\n");
 	printf("%s", get_next_line(fd));
-	printf("\nterceira parte:\n");
-	printf("%s", get_next_line(fd));
+//	printf("\nterceira parte:\n");
+//	printf("%s", get_next_line(fd));
+	close(fd);
 }
