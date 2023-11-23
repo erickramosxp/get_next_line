7b3c21385ebc585defb7e8a6aad694cb8c1b68e6
erickramosxp <erickramossilva@gmail.com>
2023-11-23 14:58:21 -0300

    fix

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: erramos <erramos@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 16:32:11 by erramos           #+#    #+#             */
-/*   Updated: 2023/11/22 18:30:39 by erramos          ###   ########.fr       */
+/*   Updated: 2023/11/23 14:55:47 by erramos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,32 +27,37 @@ int     check_backslash(char *buff)
         }
         return (0);
 }
-
+/*
 void	ft_copy(char *line, char *temp)
 {
 	int	i;
+	int	len;
 
 	i = 0;
+//	printf("%s\n\n", temp);
+	len = ft_strlen(line);
 	while (temp[i] != '\0')
 	{
-		line[i] = temp[i];
+		line[len + i] = temp[i];
 		i++;
 	}
+//	printf("%s\n\n", line);
 }
-
+*/
 char	*convert(char *line, char *buff)
 {
 	char	*temp;
 
 	if (!line)
-		ft_strdup(buff);
+	{
+		line = ft_strdup(buff);
+//		printf("%s", line);
+	}
 	else
 	{
 		temp = ft_strdup(line);
 		free(line);
-		line = (char *)malloc(ft_strlen(temp) + ft_strlen(buff) * sizeof(char));
-		ft_copy(line, temp);
-		ft_copy(line, buff);
+		line = ft_strjoin(temp, buff);
 		free(temp);
 	}
 	return (line);
@@ -97,11 +102,12 @@ char	*remove_linebreak(char *line)
 	{
 		line[j] = temp[i];
 		i++;
+		j++;
 	}
 	free(temp);
 	return (line);
 }
-
+/*
 char	*transfer(int fd, char *buff, char *line)
 {
 	char	*resp;
@@ -118,13 +124,14 @@ char	*transfer(int fd, char *buff, char *line)
 		if (check_backslash(line))
 		{
 			resp = new_line(resp, line);
-			line = remove_linebreak(line);
+//			line = remove_linebreak(line);
+//			printf("\n\n%s\n\n", line);
 			check = 0;
 		}
 	}
 	return (resp);
 }
-
+*/
 char	*get_next_line(int fd)
 {
 	char	*buff;
@@ -134,7 +141,30 @@ char	*get_next_line(int fd)
 	buff = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buff)
 		return (NULL);
-	resp = transfer(fd, buff, line);
+//	resp = transfer(fd, buff, line);
+//
+
+//	char    *resp;
+        int     rd;
+        int     check;
+
+        rd = 1;
+        check = 1;
+        while (rd > 0 && check)
+        {
+                rd = read(fd, buff, BUFFER_SIZE);
+                buff[rd] = '\0';
+                line = convert(line, buff);
+                if (check_backslash(line))
+                {
+                        resp = new_line(resp, line);
+                   	line = remove_linebreak(line);
+//                      printf("\n\n%s\n\n", line);
+                        check = 0;
+                }
+        }
+//	printf("\n\n%s\n\n", line);
+//	printf("%s", line);
 	return (resp);
 }
 
@@ -144,9 +174,9 @@ int	main(void)
 
 	fd = open("text.txt", O_RDONLY);
 	printf("Primeira parte:\n%s",  get_next_line(fd));
-//	printf("\nsegunda parte:\n");
-//	printf("%s", get_next_line(fd));
-//	printf("\nterceira parte:\n");
-//	printf("%s", get_next_line(fd));
+	printf("\nsegunda parte:\n");
+	printf("%s", get_next_line(fd));
+	printf("\nterceira parte:\n");
+	printf("%s", get_next_line(fd));
 //	close(fd);
 }
